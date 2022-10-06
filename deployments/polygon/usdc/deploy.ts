c05f7340fa0f3471df77b81fa08ae2591134d83f
Scott Silver <scott@compound.finance>
2022-10-06 15:20:21 -0700

    initial config and deploy script

--- deployments/polygon/usdc/deploy.ts
+++ deployments/polygon/usdc/deploy.ts
@@ -1,30 +1,71 @@
 import { Deployed, DeploymentManager } from '../../../plugins/deployment_manager';
 import { DeploySpec, deployComet } from '../../../src/deploy';
 
+const secondsPerDay = 24 * 60 * 60;
+
+const MAINNET_TIMELOCK = '0x6d903f6003cca6255d85cca4d3b5e5146dc33925';
+
 export default async function deploy(deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed> {
+  const trace = deploymentManager.tracer()
+
   // pull in existing assets
-  //   WETH
-  //   WBTC
-  //   MATIC
-  //   USDC
-  //   DAI
-  //   USDT
+  const USDC = await deploymentManager.existing('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174');
+  const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619');
+  const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6');
+  const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270')
+  const DAI = await deploymentManager.existing('DAI', '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063');
+  const USDT = await deploymentManager.existing('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f');
+
+  const fxChild = await deploymentManager.existing('fxChild', '0x8397259c983751DAf40400790063935a11afa28a');
 
   // Deploy PolygonBridgeReceiver
+  const bridgeReceiver = await deploymentManager.deploy(
+    'bridgeReceiver',
+    'bridges/polygon/PolygonBridgeReceiver.sol',
+    [fxChild?.address]  // fxChild
+  );
 
   // Deploy Local Timelock
+  const localTimelock = await deploymentManager.deploy(
+    'timelock',
+    'vendor/Timelock.sol',
+    [
+      bridgeReceiver.address, // admin
+      2 * secondsPerDay,      // delay
+      14 * secondsPerDay,     // grace period
+      2 * secondsPerDay,      // minimum delay
+      30 * secondsPerDay      // maxiumum delay
+    ]
+  );
 
-  // Initialize bridge receiver
+  // Initialize PolygonBridgeReceiver
+  await deploymentManager.idempotent(
+    async () => !(await bridgeReceiver.initialized()),
+    async () => {
+      trace(`Initializing BridgeReceiver`);
+      await bridgeReceiver.initialize(
+        MAINNET_TIMELOCK,       // govTimelock
+        localTimelock.address  // localTimelock
+      );
+      trace(`BridgeReceiver initialized`);
+    }
+  );
 
   // Deploy Comet
+  const deployed = await deployComet(deploymentManager, deploySpec);
+  const { comet } = deployed;
 
-  // Deploy Bulker
+  // Deploy Polygon Bulker
+  // XXX implement Polygon Bulker
+  // const bulker = await deploymentManager.deploy(
+  //   'bulker',
+  //   'Bulker.sol',
+  //   [await comet.governor(), WETH.address]
+  // );
 
   return {
-    /*
-    deployed
-    bulker
+    ...deployed,
     bridgeReceiver
-    */
+    // bulker
   };
 }
