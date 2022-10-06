2cb82883fee9831ddbdbfc1c1b1d765765c36042
Scott Silver <scott@compound.finance>
2022-10-06 15:35:05 -0700

    import from Polygon

--- deployments/polygon/usdc/deploy.ts
+++ deployments/polygon/usdc/deploy.ts
@@ -9,14 +9,14 @@ export default async function deploy(deploymentManager: DeploymentManager, deplo
   const trace = deploymentManager.tracer()
 
   // pull in existing assets
-  const USDC = await deploymentManager.existing('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174');
-  const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619');
-  const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6');
-  const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270')
-  const DAI = await deploymentManager.existing('DAI', '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063');
-  const USDT = await deploymentManager.existing('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f');
+  const USDC = await deploymentManager.existing('USDC', '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', 'polygon');
+  const WETH = await deploymentManager.existing('WETH', '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', 'polygon');
+  const WBTC = await deploymentManager.existing('WBTC', '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 'polygon');
+  const WMATIC = await deploymentManager.existing('WMATIC', '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', 'polygon')
+  const DAI = await deploymentManager.existing('DAI', '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', 'polygon');
+  const USDT = await deploymentManager.existing('USDT', '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', 'polygon');
 
-  const fxChild = await deploymentManager.existing('fxChild', '0x8397259c983751DAf40400790063935a11afa28a');
+  const fxChild = await deploymentManager.existing('fxChild', '0x8397259c983751DAf40400790063935a11afa28a', 'polygon');
 
   // Deploy PolygonBridgeReceiver
   const bridgeReceiver = await deploymentManager.deploy(
